fb8f33897e4be9ecba39471a594964a6fc3500a3
ANIKET KADALE <aniketkadale18@gmail.com>
2023-02-12 22:08:58 +0530

    Adding files of the project

---
+++ app/src/main/java/com/example/explicitintent/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.explicitintent
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import com.example.explicitintent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnIntent = findViewById<Button>(R.id.button)
+
+        btnIntent.setOnClickListener {
+            //we want to open a new activity
+            intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+}
